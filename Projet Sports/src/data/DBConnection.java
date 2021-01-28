15052cb79bced947ef9aec1d81052983238bb343
serMPO <seruche01@gmail.com>
2021-01-28 02:12:09 +0100

    News

---
+++ Projet Sports/src/data/DBConnection.java
@@ -0,0 +1,45 @@
+package data;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class DBConnection {
+	private static SessionFactory sessionFactory;
+	private static AnnotationConfiguration config;
+
+	public static AnnotationConfiguration getConfig() {
+		if (config == null) {
+			config = new AnnotationConfiguration();
+			config.addAnnotatedClass(User.class);
+			config.addAnnotatedClass(Friends.class);
+			config.addAnnotatedClass(Sport.class);
+			config.addAnnotatedClass(Aviron.class);
+			config.addAnnotatedClass(Escalade.class);
+			config.addAnnotatedClass(Musculation.class);
+			config.addAnnotatedClass(TirArc.class);
+			config.addAnnotatedClass(Jogging.class);
+			config.addAnnotatedClass(Natation.class);
+			String packageName = DBConnection.class.getPackage().getName();
+			config.configure(packageName + "/connection.cfg.xml");
+		}
+		return config;
+	}
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				AnnotationConfiguration config = getConfig();
+				sessionFactory = config.buildSessionFactory();
+			} catch (Throwable ex) {
+				System.err.println("Initial SessionFactory creation failed." + ex);
+				throw new ExceptionInInitializerError(ex);
+			}
+		}
+		return sessionFactory;
+	}
+
+	public static Session getSession() {
+		return getSessionFactory().openSession();
+	}
+}
